4a5275d4765a4820b793e6f815fb3f7cadcba13c
Udhaya Moorthy <149321495+Udhays07@users.noreply.github.com>
2025-01-22 16:57:44 +0530

    Time: 452 ms (5.01%), Space: 85.9 MB (5.01%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.java
+++ 0817-design-hashmap/0817-design-hashmap.java
@@ -1,44 +1,20 @@
 class MyHashMap {
-    // List of key-value pairs
-    public ArrayList<ArrayList<Integer>> list;
-
+    public int[] arr;
     public MyHashMap() {
-        list = new ArrayList<>();
+        arr = new int[10000001];
+        Arrays.fill(arr, -1);
     }
-
-    // Put key-value pair into the map
+    
     public void put(int key, int value) {
-        // First, check if the key already exists, if so, update its value
-        for (ArrayList<Integer> innerList : list) {
-            if (innerList.get(0) == key) {
-                innerList.set(1, value);
-                return;
-            }
-        }
-        // If key doesn't exist, add a new key-value pair
-        list.add(new ArrayList<>(List.of(key, value)));
+        arr[key] = value;
     }
-
-    // Get the value associated with the key
+    
     public int get(int key) {
-        for (ArrayList<Integer> innerList : list) {
-            if (innerList.get(0) == key) {
-                return innerList.get(1); // Return the value
-            }
-        }
-        return -1; // If key not found, return -1
+        return arr[key];
     }
-
-    // Remove the key-value pair from the map
+    
     public void remove(int key) {
-        int i = 0;
-        for (ArrayList<Integer> innerList : list) {
-            if (innerList.get(0) == key) {
-                list.remove(i); // Remove the key-value pair
-                return;
-            }
-            i++;
-        }
+        arr[key] = -1;
     }
 }
 
