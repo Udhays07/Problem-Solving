1f38ed5546c5f5f929f3dd9951322fe5421a4191
Udhaya Moorthy <149321495+Udhays07@users.noreply.github.com>
2025-01-22 11:59:48 +0530

    Time: 10 ms (5.14%), Space: 61.1 MB (5.04%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int result = -1;
+        int n = gas.length;
+        int totalgas = 0, totalcost = 0, startindex = 0, currentgas = 0;
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0; i<n; i++){
+            totalgas += gas[i];
+            totalcost += cost[i];
+            currentgas += (gas[i] - cost[i]);
+            q.add(gas[i] - cost[i]);
+            while(currentgas < 0 && !q.isEmpty()){
+                currentgas -= q.poll();
+                startindex++;
+            }
+        }
+        return totalgas >= totalcost ? startindex % n : -1;
+    }
+}
