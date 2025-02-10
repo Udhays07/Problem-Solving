a7a5ef9f179732e9ed345f8b1a15a3ab30b34006
Udhaya Moorthy <149321495+Udhays07@users.noreply.github.com>
2025-02-10 10:08:10 +0530

    Time: 8 ms (28.29%), Space: 40.6 MB (96.72%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,11 +1,11 @@
 class Solution {
     public int arrangeCoins(int n) {
         int ans = 0;
-        for(int i=1; i<=n; i++){
-            if(n >= i){
-                n -= i;
-                ans++;
-            }
+        int i = 1;
+        while(n >= i){
+            n -= i;
+            ans++;
+            i++;
         }   
         return ans;
     }
