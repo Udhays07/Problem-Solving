e796a7bdad227db081190b09c0ebbc14938e9d1b
Udhaya Moorthy <149321495+Udhays07@users.noreply.github.com>
2025-03-05 15:17:23 +0530

    Time: 2 ms (99.74%), Space: 46.4 MB (96.23%) - LeetHub

---
+++ 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] sortArrayByParityII(int[] nums) {
+        int[] arr = new int[nums.length];
+        int odd = 1, even = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i]%2 == 0){
+                arr[even] = nums[i];
+                even += 2;
+            }else{
+                arr[odd] = nums[i];
+                odd += 2;
+            }
+        }
+        return arr;
+    }
+}
